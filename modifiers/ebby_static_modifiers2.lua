e4ea54a3aae0f1b75039eb54a4a95087dd8cf8c9
0kib1 <97839762+0kib1@users.noreply.github.com>
2023-05-19 21:29:20 +0300

    2

---
+++ modifiers/ebby_static_modifiers2.lua
@@ -0,0 +1,23 @@
+# targeted modifiers that will apply when you have fully decrypted a country's cipher
+passive_decryption_modifier = {
+	air_detection = 0.05
+	air_intercept_efficiency = 0.25
+	civilian_intel_decryption_bonus = 25
+	army_intel_decryption_bonus = 25
+	navy_intel_decryption_bonus = 25
+	airforce_intel_decryption_bonus = 25
+}
+
+# targeted modifiers that will apply when you have activated your active decryption bonuses against a country
+active_decryption_modifier = {
+	# attack_bonus_against = 0.15
+	defense_bonus_against = 0.15
+	breakthrough_bonus_against = 0.15
+	naval_invasion_prep_speed = 0.5
+	naval_invasion_penalty = -0.15
+	planning_speed = 0.5
+	civilian_intel_decryption_bonus = 100
+	army_intel_decryption_bonus = 100
+	navy_intel_decryption_bonus = 100
+	airforce_intel_decryption_bonus = 100
+}
